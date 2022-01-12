41a902fd2333e3f716d544cc16b9694171923275
Sezgin Erdem, Ph.D <67353158+sezginerdem@users.noreply.github.com>
2022-01-12 15:04:22 +0000

    Update variables.tf

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -1,9 +1,9 @@
 variable "region" {
-  default = "eu-west-2"
+  default = eu-west-2
 }
 
 variable "ami_id" {
-  type = "map"
+  type = map
 
   default = {
     eu-west-2    = "ami-0015a39e4b7c0966f"
