4be16a4fb38d91e2435ef6e6a6a21e401c463a55
Sezgin Erdem, Ph.D <67353158+sezginerdem@users.noreply.github.com>
2022-01-12 15:19:07 +0000

    Update aws-instance-example.tf

--- aws-instance-first-script/aws-instance-example.tf
+++ aws-instance-first-script/aws-instance-example.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web1" {
-   ami           = var.ami_id
-   region        = var.region
+   ami           = "ami-0015a39e4b7c0966f"
+   region        = "eu-west-2"
    instance_type = "t2.micro"
  }
